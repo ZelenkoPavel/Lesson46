e5d2efa364b481c401e0afafee4ea091ca1a87d3
ITSTEP\st <st>
2024-03-05 17:52:39 +0300

    .

--- Project1/Task01.cpp
+++ Project1/Task01.cpp
@@ -5,7 +5,25 @@ using namespace std;
 
 int main() {
 
+	int n = 5;
+	int m = 10;
 
+	int** classes = new int* [n];
+
+	for (int i = 0; i < n; i++)
+	{
+		*(classes + i) = new int[m];
+		//classes[i] = new int[m];
+	}
+
+
+	for (int j = 0; j < n; j++)
+	{
+		delete[] *(classes + j);  // power
+		//delete[] classes[j];
+	}
+
+	delete[] classes;
 
 	return 0;
 }
